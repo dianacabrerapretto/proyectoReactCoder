ca4c0181863b02d7a66fb9521d8df57a6ef498f3
Diana Cabrera <lic.cabreradiana@gmail.com>
2022-04-14 11:14:52 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home"
 
 const App = () => {
+  return (
+      <BrowserRouter>
+          <NavBar />
+          <Routes>
+              <Route path='/' element={<ItemListContainer />} />
+              <Route path='/category/:idCategory' element={<ItemListContainer />} />
+              <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+          </Routes>
+      </BrowserRouter>
+  );
+}
+
+        /*
+const App = () => {
   return <Home />;
 };
+*/
 
 export default App;
