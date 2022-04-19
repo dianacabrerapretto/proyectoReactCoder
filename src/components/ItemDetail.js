8ae9d64efa91f520716d644d1eee1d51a11eecd6
Diana Cabrera <lic.cabreradiana@gmail.com>
2022-04-19 10:20:51 -0300

    modificaciones

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,14 +1,18 @@
 import ItemCount from './ItemCount';
-import CheckOut from "./Checkout";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { Link } from 'react-router-dom';
+import { CartContext } from './CartContext';
+import { Button } from '@material-ui/core';
 import { MainContainer, BoxDetail, ImageBox, ImageDetail, DetailContainer, Title, Desc, Price } from './Styles';
 
 const ItemDetail = ({ item }) => {
     const [itemCount, setItemCount] = useState(0);
+    const test = useContext(CartContext);
 
     const onAdd = (qty) => {
         alert(qty + " items agregados al carrito");
         setItemCount(qty);
+        test.addToCart(item, qty);
     }
 
     return (
@@ -27,10 +31,11 @@ const ItemDetail = ({ item }) => {
                                 <Price>$ {item.cost}</Price>
                                 <Desc>{item.stock} unidades en stock</Desc>
                             </DetailContainer>
-                            {   ItemCount === 0
-                                        ?<ItemCount stock={item.stock} initial={0} onAdd={onAdd}/>
-                                        :<CheckOut />
-                                    }
+                            {
+                        itemCount === 0
+                        ? <ItemCount stock={item.stock} initial={itemCount} onAdd={onAdd} />
+                        : <Link to='/cart' style={{textDecoration: "none"}}><Button variant="contained" color="secondary">CheckOut</Button></Link>
+                    }
                         </BoxDetail>
                     </MainContainer>
                     : <p>Cargando...</p>
